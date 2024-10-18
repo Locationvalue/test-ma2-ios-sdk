ee9b19bd899e7bcd833a8aa86bbb4f83f97252eb
masuno <masuno@PC-375.local>
2024-10-18 11:28:38 +0900

    SDK1.14.0, dSYMあり

---
+++ NautilusCampaignSDK.xcframework/ios-arm64/NautilusCampaignSDK.framework/Headers/NautilusCampaign.h
@@ -0,0 +1,17 @@
+//
+//  NautilusCampaign.h
+//  NautilusCampaignSDK
+//
+//  Created by shunsuke.miyano on 2024/08/08.
+//  Copyright © 2024 LocationValue Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NautilusCampaign
+FOUNDATION_EXPORT double NautilusCampaignVersionNumber;
+
+//! Project version string for NautilusCampaign
+FOUNDATION_EXPORT const unsigned char NautilusCampaignVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NautilusCampaign/PublicHeader.h>
